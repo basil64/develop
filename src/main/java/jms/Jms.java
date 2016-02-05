ea007a293c19a4f48e6274ea94da91c5019eca55
Piotr Ziembicki <pzmz@poczta.onet.pl>
2016-02-05 22:10:45 +0100

    New comment and system output

--- src/main/java/jms/Jms.java
+++ src/main/java/jms/Jms.java
@@ -2,7 +2,9 @@ package jms;
 
 public class Jms {
 
+    // This is first class
     public static void main(String[] argv) {
         System.out.println("This is test");
+        System.out.println("This is next test");
     }
 }
